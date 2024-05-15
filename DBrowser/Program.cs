92c8c6a543e3b4653ea4389e2e50e5d496a1f644
Николай Махов <nmahov2003@gmail.com>
2024-05-15 11:25:17 +0700

    Вернул обратно старый buildServiceProvider (его надо переделать)

--- DBrowser/Program.cs
+++ DBrowser/Program.cs
@@ -1,5 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
-using PluginBase.QueryPlan;
+using Service.ConnectionService;
+using Service.Logger;
+using Service.QueryExecutor;
+using Service.QueryParser;
+using Service.QueryPlan;
+using Service.Transaction;
+using Service.TransactionManager;
+using Service;
 
 namespace DBrowser
 {
@@ -14,7 +21,41 @@ namespace DBrowser
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //Application.Run(new Form1(buildServiceProvider()));
+            Application.Run(new Form1(buildServiceProvider()));
+        }
+
+        static ServiceProvider buildServiceProvider()
+        {
+            return new ServiceCollection()
+            .AddSingleton<IQueryPlanAnalyzer, SqliteQueryPlanAnalyzer>()
+            .AddSingleton(
+                factory => (Func<DbName, IQueryPlanAnalyzer?>)
+                    (key => factory.GetServices<IQueryPlanAnalyzer>().FirstOrDefault(o => o.Name == key))
+            )
+            .AddSingleton<IQueryExecutor, SqliteQueryExecutor>()
+            .AddSingleton(
+                factory => (Func<DbName, IQueryExecutor?>)
+                    (key => factory.GetServices<IQueryExecutor>().FirstOrDefault(o => o.Name == key))
+            )
+            .AddSingleton<IQueryParser, SqliteQueryParser>()
+            .AddSingleton(
+                factory => (Func<DbName, IQueryParser?>)
+                    (key => factory.GetServices<IQueryParser>().FirstOrDefault(o => o.Name == key))
+            )
+            .AddSingleton<IConnectionService, SqliteConnectionService>()
+            .AddSingleton(
+                factory => (Func<DbName, IConnectionService?>)
+                    (key => factory.GetServices<IConnectionService>().FirstOrDefault(o => o.Name == key))
+            )
+            .AddSingleton<ITransactionManager, TransactionManager>()
+            .AddSingleton<ITransactionExecutor, SqliteTransactionExecutor>()
+            .AddSingleton(
+                factory => (Func<DbName, ITransactionExecutor?>)
+                    (key => factory.GetServices<ITransactionExecutor>().FirstOrDefault(o => o.Name == key))
+            )
+            .AddSingleton<IQueryLogger, QueryLogger>()
+            .AddSingleton<ICsvExporter, CsvExporter>()
+            .BuildServiceProvider();
         }
     }
 }
